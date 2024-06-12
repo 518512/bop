127f3317a0481d2865a5310762498901db485db6
Up <8566666@gmail.com>
2024-06-12 17:17:33 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -154,13 +154,13 @@ config_package_add luci-app-mosdns
 config_package_add mosdns
 
 # add turboacc  待恢复20240611测试不是此项影响-01
-# config_package_add luci-app-turboacc
-# config_package_add luci-app-turboacc
-# config_package_add luci-app-turboacc_INCLUDE_OFFLOADING
-# config_package_add luci-app-turboacc_INCLUDE_DNSFORWARDER
-# config_package_add luci-app-turboacc_INCLUDE_DNSPROXY
-# config_package_add luci-app-turboacc_INCLUDE_SHORTCUT_FE
-# config_package_add luci-app-turboacc_INCLUDE_SHORTCUT_FE_CM
+config_package_add luci-app-turboacc
+config_package_add luci-app-turboacc
+config_package_add luci-app-turboacc_INCLUDE_OFFLOADING
+config_package_add luci-app-turboacc_INCLUDE_DNSFORWARDER
+config_package_add luci-app-turboacc_INCLUDE_DNSPROXY
+config_package_add luci-app-turboacc_INCLUDE_SHORTCUT_FE
+config_package_add luci-app-turboacc_INCLUDE_SHORTCUT_FE_CM
 
 # add oc wg zt
 config_package_add luci-app-wireguard
